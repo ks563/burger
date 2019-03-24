22d229cde0dca2e6929af2e29c0fc6052f0d45dc
ks563 <karen.shea90@gmail.com>
2019-03-24 07:43:00 -0500

    file structure

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Cali Burger", "False");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", "False");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Classic Burger", "False");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cali Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Classic Burger", False);
