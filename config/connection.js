190fafa6248bc4e3d670247db4b44fead4b01747
ks563 <karen.shea90@gmail.com>
2019-04-13 18:27:17 -0500

    heroku jaws update

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 //sets up connection to myswl
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+var connection =
+    
+    if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    
+        mysql.createConnection({
+            host: "127.0.0.1",
+            port: 3306,
+            user: "root",
+            password: "password",
+            database: "burgers_db",
+        });
+};
+
 
 //establishes connection
 
